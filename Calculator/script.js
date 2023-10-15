a8fafb61276064cb919e91ece7b5e27b53060b8f
Ayush9034 <147938892+Ayush9034@users.noreply.github.com>
2023-10-15 15:35:32 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+let currentInput = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById('result').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    try {
+        currentInput = eval(currentInput);
+        document.getElementById('result').value = currentInput;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+        currentInput = '';
+    }
+}
